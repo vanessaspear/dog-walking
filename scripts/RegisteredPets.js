c2f49a9ed82daf9ec883162bbf2b93321b393555
Vanessa Spear <vanessavspear@gmail.com>
2022-10-11 14:39:21 -0500

    added event listeners to RegisteredPets.js and Walkers.js

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,16 +1,27 @@
 import { getPets } from "./database.js"
 
+document.addEventListener('click', e => {
+    const elementClicked = e.target;
+    if (elementClicked.id.startsWith("pet")) {
+        const [, petPrimaryKey] = elementClicked.id.split("--");
+        for (let pet of pets) {
+            if (pet.id === parseInt(petPrimaryKey)) {
+                window.alert(`${pet.name} barks at you`);
+            }         
+        }
+    }
+})
+
 const pets = getPets()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
 
     return petHTML
 }
-
